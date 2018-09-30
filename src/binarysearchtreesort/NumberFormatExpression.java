3c6476bc4fddb92bd9ba56e724946247b7d4f27c
luke <luke@gmail.com>
2018-09-29 21:45:05 -0400

    commit Number Formatting Expressions

---
+++ src/binarysearchtreesort/NumberFormatExpression.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtreesort;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ldcha
+ */
+public class NumberFormatExpression {
+    //throw error message for invalid inputs
+public static void numberError(){
+        JOptionPane.showMessageDialog(null, "Invalid Input Detected.  Please"
+                + "double check input and try again", "Whoa There", JOptionPane.WARNING_MESSAGE);
+}
+}
